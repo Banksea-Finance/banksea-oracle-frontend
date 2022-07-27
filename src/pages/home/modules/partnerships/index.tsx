26039335b2f45c1f858d75f4c8345d2dcde18a7c
disperito <50449082+Disperito@users.noreply.github.com>
2022-07-27 11:21:14 +0800

    feat: update partnerships

--- src/pages/home/modules/partnerships/index.tsx
+++ src/pages/home/modules/partnerships/index.tsx
@@ -11,33 +11,69 @@ export type Partnership = {
 
 const PARTNERSHIPS: Partnership[] = [
   {
-    image: 'https://parrot.fi/icons/parrot-logo-d.svg',
-    url: 'https://parrot.fi',
+    image: require('@/assets/images/pages/home/partnerships/slope.png'),
+    url: 'https://slope.finance/',
+    scale: 0.6
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/Bitkeep.png'),
+    url: 'https://bitkeep.com/',
+    scale: 0.7
+
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/XCarnival.png'),
+    url: 'https://xcarnival.fi/',
+    scale: 0.6
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/Certik.svg').default,
+    url: 'https://www.certik.com/',
+    scale: 0.4
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/Frakt.png'),
+    url: 'https://frakt.xyz/',
+    scale: 0.6
   },
   {
     image: require('@/assets/images/pages/home/partnerships/moonbeam.png'),
     url: 'https://moonbeam.network/',
+    scale: 0.85
   },
   {
-    image: require('@/assets/images/pages/home/partnerships/slope.png'),
-    url: 'https://slope.finance/',
-    scale: 0.7
+    image: require('@/assets/images/pages/home/partnerships/Droid.png'),
+    url: 'https://droidcapital.net/',
+    scale: 2
   },
   {
-    image: require('@/assets/images/pages/home/partnerships/solanart.png'),
-    url: 'https://solanart.io/collections/citizenone',
-    text: 'Solanart',
-    scale: 0.9
-  },
-  {
-    image: require('@/assets/images/pages/home/partnerships/ME.svg').default,
-    url: 'https://magiceden.io/marketplace/citizenone',
+    image: require('@/assets/images/pages/home/partnerships/parrot.svg').default,
+    url: 'https://parrot.fi',
     scale: 0.8
   },
   {
-    image: 'https://solsea.io/assets/solsea_Logo_RGB_final1.svg',
-    url: 'https://solsea.io/collection/61f56d3d6f589f689cbe1c18',
-    scale: 0.7
+    image: require('@/assets/images/pages/home/partnerships/zebec.png'),
+    url: 'https://zebec.io/',
+    scale: 0.8
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/SolSea.svg').default,
+    url: 'https://solsea.io/',
+    scale: 0.55
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/exomon.png'),
+    url: 'https://www.exomon.xyz/',
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/bitcoinaddict.svg').default,
+    url: 'https://bitcoinaddict.org/',
+    scale: 0.8
+  },
+  {
+    image: require('@/assets/images/pages/home/partnerships/CryptoTimes.png'),
+    url: 'https://www.cryptotimes.io/',
+    scale: 0.45
   },
 ]
 
